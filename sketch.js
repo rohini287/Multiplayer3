3cb9be9cdb9d63cd7b58df555406d795ab3a896d
rohini287 <74919500+rohini287@users.noreply.github.com>
2021-01-29 21:20:40 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var form,player,game;
+var database
+var gameState=0
+var playerCount=0;
+var allPlayers;
+var car1,car2,car3,car4,cars=[]
+
+function setup(){
+    createCanvas(displayWidth,displayHeight);
+    database=firebase.database();
+    game=new Game();
+    game.getState()
+    game.start();
+}
+function draw() {
+    if (playerCount===4){
+        game.update(1);
+    
+    }
+    if(gameState===1){
+        clear()
+        game.play();
+    }
+    
+}
